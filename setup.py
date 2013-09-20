1b81e85d07742d1edde475ee3c50f31a3b029aa5
Tessa Lau <tlau@tlau.org>
2013-09-20 13:53:30 -0700

    Common dotfiles

---
+++ setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+# Copied from https://github.com/jonbinney/jdb-config, thanks Binney!
+
+import os, os.path, re, shutil, time
+
+config_dir = os.path.split(os.path.realpath(__file__))[0]
+
+def link_in_home(from_file, to_file):
+    from_file = os.path.join(config_dir, from_file)
+    to_file = os.path.join(home_dir, to_file)
+
+    try:
+        # delete any existing file
+        if os.path.lexists(to_file):
+            print "Deleting existing file %s" % to_file
+            os.unlink(to_file)
+
+        os.symlink(from_file, to_file)
+    except OSError as e:
+        print to_file, 'cannot be replaced'
+        print e
+    print 'Linked %s to %s' % (from_file, to_file)
+
+def insert_text_in_file(filename, text, comment_char):
+    try:
+        f_str = open(filename, 'rw').read()
+        if not re.search(r'JDB', f_str):
+            print 'Adding custom text to %s' % filename
+            shutil.copy(filename, filename + '.bak_%f' % time.time())
+            f = open(filename, 'a')
+            print >> f, ''
+            print >> f, comment_char * 78
+            print >> f, '%s Added by my setup script - JDB' % comment_char
+            print >> f, text
+            print >> f, comment_char * 78
+            f.close()
+    except:
+        print 'Unable to add custom text to %s' % filename
+
+if __name__ == '__main__':
+    home_dir = os.path.expanduser('~')
+    print 'Home dir: %s' % home_dir
+    print 'Config dir: %s' % config_dir
+    
+    # make symlinks in home directory to config files
+#    link_in_home('ssh/config', '.ssh/config')
+#    link_in_home('screen/.screenrc', '.screenrc')
+    link_in_home('vim/.vimrc', '.vimrc')
+    link_in_home('vim/.gvimrc', '.gvimrc')
+    link_in_home('vim', '.vim')
+    link_in_home('tmux/tmux.conf', '.tmux.conf')
+    link_in_home('bc/.bcrc', '.bcrc')
+    link_in_home('zsh/.zshrc', '.zshrc')
+    link_in_home('zsh', '.zsh')
+#    link_in_home('roxterm', '.config/roxterm.sourceforge.net')
+
+    # have ipython call my stuff on startup
+    #  needs to be updated for new ipython config structure...
+    #ipythonrc_filename = os.path.join(home_dir, '.ipython/ipythonrc')
+    #text = 'execfile %s' % os.path.join(home_dir, 'config/ipython/startup')
+    #insert_text_in_file(ipythonrc_filename, text, '#')
+
+    # have bash call my stuff on startup
+#    bashrc_filename = os.path.join(home_dir, '.bashrc')
+#    text = 'source %s' % os.path.join(home_dir, 'config/bash/bashrc')
+#    insert_text_in_file(bashrc_filename, text, '#')
+    
---
+++ zsh/.zshrc
@@ -0,0 +1,88 @@
+PROMPT='%m:%~%% '
+RPROMPT='%@%(0?,, %?)'
+export CVS_RSH=ssh
+export VISUAL=/usr/bin/vim
+export EDITOR=/usr/bin/vim
+export MHN=/home/tlau/Mail/mhn.defaults
+export CDPATH=.:/home/tlau
+# Less options
+export LESS=-erMMj3
+export LESSKEY=/home/tlau/.less
+export TZ=US/Pacific
+export MAIL=/home/tlau/Maildir/
+
+# Filename completion
+export FIGNORE=aux
+
+# print to molly's printer
+export PRINTER=molly
+
+bindkey -v
+bindkey -a K history-beginning-search-backward
+bindkey -a J history-beginning-search-forward
+bindkey -a '^U' kill-whole-line
+bindkey '[A' up-history
+bindkey '[B' down-history
+bindkey  up-history
+bindkey  down-history
+#stty erase '^H'
+
+# Aliases
+alias f=finger
+if [ "x$TERM" != "xtgtelnet" ]; then
+	alias ls='ls -FG'
+fi
+alias ll='ls -l'
+alias wh='ps auxw | grep'
+alias m=make
+alias library='telnet -l leo nyplgate.nypl.org'
+alias history='fc -dl'
+alias kl='tail -30 ~/.gale/log/privlog'
+alias kll='less ~/.gale/log/privlog'
+
+alias doman='nroff -man'
+
+alias bc='bc /Users/tlau/.bcrc'
+
+autoload -U compinit && compinit
+setopt EXTENDED_GLOB
+
+
+# Obsolete stuff
+#export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/local/lib:/uns/lib:/scratch/tlau/mysql/lib/mysql:/scratch/tlau/lib
+#export STAR_SPOOL_DIR=/scratch/tlau/tmp
+
+#uname=`uname`
+#export PATH=/scratch/tlau/bin:~/bin:~/bin/$uname:/uns/gcc/bin:/uns/bin:/usr/ucb:/bin:/usr/sbin:/usr/local/bin:/usr/bin:/usr/local/mh/bin:/usr/local/bin/mh:/usr/bin/X11:/uns/share/bin:/etc:/usr/etc:/usr/local/etc:/uns/trial/bin:/usr/athena/bin:/usr/local/jdk1.2/bin:.
+
+# For LaTeX
+#export TEXFONTS=:/uns/share/lib/texmf/fonts
+
+#alias uwlib='telnet uwin.u.washington.edu 1218'
+#alias smbc='smbclient \\\\rfilesrv2\\students -U tlau -W cseresearch'
+#hgrep() { grep "$@" /cse/lab/labdata/host-rec }
+
+# For spamprobe
+#export LD_LIBRARY_PATH=/home/egnor/.toast/armed/lib
+#export PATH=${PATH}:/home/egnor/.toast/armed/bin
+
+#export PYTHONPATH=${HOME}/lib/python2.3/site-packages
+
+xtermtitle() {
+	echo -n "\e]0;$*\007"
+}
+
+alias ts='ssh ofb.net'
+
+# for github
+source ~/.zsh/git-prompt/zshrc.sh
+PROMPT='%m:%~$(git_super_status)%% '
+
+# Add my bin directory to the PATH
+# Add nethack
+# Also add homebrew directories
+typeset -U path
+path=(~/bin /usr/local/bin /usr/games/bin /usr/local/share/python $path)
+export PATH
+
+export PYTHONPATH=/usr/local/lib/python2.7/site-packages:${PYTHONPATH}
